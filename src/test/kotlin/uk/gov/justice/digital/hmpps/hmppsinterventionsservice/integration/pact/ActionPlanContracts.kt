764aacbf0be69fcd13cee006c56743c312b98798
Tom Myers <tom.myers@digital.justice.gov.uk>
2021-04-13 15:41:10 +0100

    add contract state for testing session feedback submission

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/integration/pact/ActionPlanContracts.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsinterventionsservice/integration/pact/ActionPlanContracts.kt
@@ -83,4 +83,14 @@ class ActionPlanContracts(private val setupAssistant: SetupAssistant) {
       )
     )
   }
+
+  @State("an action plan with ID 0f5afe04-e323-4699-9423-fb6122580638 exists with 1 appointment with recorded attendance and behaviour")
+  fun `create an action plan with an appointment that has behaviour and attendance`() {
+    val actionPlan = setupAssistant.createActionPlan(id = UUID.fromString("81987e8b-aeb9-4fbf-8ecb-1a054ad74b2d"), numberOfSessions = 1)
+    setupAssistant.createActionPlanAppointment(
+      actionPlan, 1, 120, OffsetDateTime.parse("2021-05-13T13:30:00+01:00"),
+      Attended.LATE, "Alex missed the bus",
+      "Alex was well behaved", false,
+    )
+  }
 }
